15fe3794987c06ad88c21fdae5dd8623e21754ea
min2oyo <min2u@outlook.com>
2022-04-20 22:59:44 +0900

    15 POST(생성), useNavigate()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./component/Header";
 import DayList from "./component/DayList";
 import Day from "./component/Day";
+import CreateWord from "./component/CreateWord";
+import CreateDay from "./component/CreateDay";
 import EmptyPage from "./component/EmptyPage";
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="/create_word" element={<CreateWord />} />
+          <Route path="/create_day" element={<CreateDay />} />
           <Route path="/*" element={<EmptyPage />} />
         </Routes>
       </div>
