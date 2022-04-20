9f3baf441c28ad50679d7ac1bcfbd06973aba0fa
min2oyo <min2u@outlook.com>
2022-04-20 20:19:51 +0900

    10 라우터 구현 react-router-dom

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,23 +1,30 @@
 import dummy from "../db/data.json";
+import { useParams } from "react-router-dom";
 
 export default function Day() {
 
-  const day = 1;
+  const { day } = useParams();
+  // const day = useParams().day; //위와 같음
+
   const wordList = dummy.words.filter(word => (
-    word.day === day
+    word.day === Number(day)
   ))
 
+
   return (
-    <table>
-      <tbody>
-        {wordList.map(word => (
-          <tr key={word.id}>
-            <td>{word.eng}</td>
-            <td>{word.kor}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <>
+      <h2>Day {day}</h2>
+      <table>
+        <tbody>
+          {wordList.map(word => (
+            <tr key={word.id}>
+              <td>{word.eng}</td>
+              <td>{word.kor}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 
 }
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,4 +1,5 @@
 import dummy from "../db/data.json";
+import { Link } from "react-router-dom";
 
 export default function DayList() {
 
@@ -6,7 +7,9 @@ export default function DayList() {
     <ul className="list_day">
       {dummy.days.map(day => (
         <li key={day.id}>
-          Day {day.day}
+          <Link to={`/day/${day.day}`}>
+            Day {day.day}
+          </Link>
         </li>
       ))}
     </ul>
