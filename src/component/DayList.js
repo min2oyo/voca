407b3860cc11d53b70719e8c91ad01c72d7fc395
min2oyo <min2u@outlook.com>
2022-04-20 21:55:34 +0900

    13 Custom Hooks

--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList() {
 
-  const [days, setDays] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:3001/days")
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setDays(data);
-      })
-  }, []);
+  const days = useFetch("http://localhost:3001/days");
 
   return (
     <>
@@ -30,50 +20,3 @@ export default function DayList() {
   )
 
 }
-
-
-
-// // useEffect의 count 실험
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-
-// export default function DayList() {
-
-//   const [days, setDays] = useState([]);
-//   const [count, setCount] = useState(0);
-
-//   function onClick1() {
-//     setCount(count + 1);
-//   }
-
-//   function onClick2() {
-//     setDays([
-//       ...days,
-//       {
-//         id: Math.random(),
-//         day: 1
-//       }
-//     ]);
-//   }
-
-//   useEffect(() => {
-//     console.log("count change");
-//   }, []);
-
-//   return (
-//     <>
-//       <ul className="list_day">
-//         {days.map(day => (
-//           <li key={day.id}>
-//             <Link to={`/day/${day.day}`}>
-//               Day {day.day}
-//             </Link>
-//           </li>
-//         ))}
-//       </ul>
-//       <button onClick={onClick1}>{count}</button>
-//       <button onClick={onClick2}>Day change</button>
-//     </>
-//   )
-
-// }
